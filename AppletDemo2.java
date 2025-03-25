eaf0840e0b68b6bd2b19bd1491183c32facd5be5
Ritheesh170 <245122748065@mvsrec.edu.in>
2025-03-25 13:30:05 +0530

    Add files via upload

---
+++ AppletDemo2.java
@@ -0,0 +1,14 @@
+import java.applet.*;
+import java.awt.*;
+
+/*<applet code="AppletDemo2.class" width="300" height="300">
+</applet>*/
+
+public class AppletDemo2 extends Applet
+{
+public void paint(Graphics g)
+{
+g.setFont(FONT.ITALIC);
+g.drawString("AIML",100,20);
+}
+}
